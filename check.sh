0ce8edff0d72c59e5dd8418525dd8062f0cf09c7
Astel <astelthottankara@gmail.com>
2021-01-09 11:57:42 +0530

    feat: display error location

--- check.sh
+++ check.sh
@@ -12,5 +12,5 @@ tot=${#out_files[@]}
 echo
 for(( i=0; i<tot; i++ ))
 do
-    diff ${out_files[i]} ${res_files[i]} && echo "Test #$i passed" || echo "Test #$i failed"
+    diff ${out_files[i]} ${res_files[i]} && echo "Test #$i passed" || (echo && cmp ${out_files[i]} ${res_files[i]} || echo "Test #$i failed")
 done
