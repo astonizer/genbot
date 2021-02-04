38d7dd58915a37c8168ad30ddd7e26eeb3b5ca40
Astel <astelthottankara@gmail.com>
2021-02-04 13:31:45 +0530

    fix: codeforces bugs

--- README.md
+++ README.md
@@ -5,14 +5,19 @@ Generate c++ templates and test case files for competitive programming contests 
 
 # Usage
 1. Clone this repo and `cd` into it.
-2. Run `node script CONTEST_URL`
-    where CONTEST_URL is the url of the form "https://codeforces.com/contest/XXXX:"
+2. Run `node script PLATFORM CONTEST_URL`
+    where CONTEST_URL is the url of the page which displays all problems.
 3. Directory will be created for each problem with its own testcase and cpp files.
     e.x. For codeforces, it would be A, B, etc.
 4. Run `code A/sol.cpp` to open the corresponding solution file and so for the others.
 5. Run `./run.sh A` after completion of solution to compare your results.
-
+<br>
+| PLATFORM | CONTEST_URL |
+|----------|-------------|
+| codeforces | "https://codeforces.com/contest/{contest_id}" |
+| codechef | "https://www.codechef.com/{contest_id}{division}" |
+| atcoder | "https://atcoder.jp/contests/{contest_id}/tasks" |
 
 Tip : Edit `template.cpp` as per your convenience
 
-Note : Functionality only for codeforces contests. More functionality will be added soon.
+Note : Functionality only for codeforces contests. More functionality will be added soon. Currently working on atcoder bot.
--- script.js
+++ script.js
@@ -1,79 +1,33 @@
 #!/usr/bin/env node
 
 const axios = require('axios');
-const cheerio = require('cheerio');
-const fs = require('fs');
+const codeforces = require('./platforms/codeforces');
+const codechef = require('./platforms/codechef');
+const atcoder = require('./platforms/atcoder');
 
-let fetchAllTestcasesOfProblem = (directory, html) => {
-    fs.copyFileSync(`${directory}/../template.cpp`, `${directory}/sol.cpp`);
-    data = [];
-    const $ = cheerio.load(html);
-    $('div.input pre').each((id, element) => {
-        data[id] = {
-            ...data[id], 
-            input: $(element).text()
-        };
-    });
-    $('div.output pre').each((id, element) => {
-        data[id] = {
-            ...data[id], 
-            output: $(element).text()
-        };
-    });
-    console.log(`Creating input and output files for ${directory.substring(2)}...`);
-    data.forEach((testCase, id) => {
-        fs.writeFile(`${directory}/in${id}.txt`, testCase.input, error => {
-            if(error) {
-                console.log(error);
-            }
-            console.log(`Input file ${directory}/in${id}.txt was saved`);
-        });
-        fs.writeFile(`${directory}/out${id}.txt`, testCase.output, error => {
-            if(error) {
-                console.log(error);
-            }
-            console.log(`Output file ${directory}/out${id}.txt was saved`);
-        });
-    });
-}
-
-// Creating directories for each problem
-let fetchAllProblemsOfContest = (problem_url) => {
-
-    // Create directory if not already exists
-    let directory = `./${problem_url.substring(problem_url.lastIndexOf('/')+1)}`;
-    if(!fs.existsSync(directory))
-        fs.mkdirSync(directory);
-    
-    // Create testcase files of given problem
-    axios.get(problem_url)
-        .then(response => {
-            fetchAllTestcasesOfProblem(directory, response.data);
-        })
-        .catch(error => {
-            console.log(error);
-        })
-};
-
-// Processing all the available problems seperately
-let fetchProblemsListFromContest = (html) => {
-    const $ = cheerio.load(html);
-    console.log("Parsing problems from contest");
-    $('tr td.id a').each((id, problem) => {
-        let problem_url = 'https://codeforces.com' + $(problem).attr('href');
-        console.log(problem_url);
-        fetchAllProblemsOfContest(problem_url);
-    })
-};
+// Accessing the contest platform
+const contest_platform = process.argv[2];
 
 // Accessing the contest url from the terminal
-const contest_url = process.argv[2];
+const contest_url = process.argv[3];
 
 // Main entry
 // Checks correctness of url provided
 axios.get(contest_url)
     .then(response => {
-        fetchProblemsListFromContest(response.data);
+        switch(contest_platform) {
+            case 'codeforces':
+                codeforces(response.data);
+                break;
+            case 'codechef':
+                codechef(response.data);
+                break;
+            case 'atcoder':
+                atcoder(response.data);
+                break;
+            default:
+                throw Error(contest_platform+ ' is not a valid platform');
+        }
     })
     .catch(error => {
         console.log(error);
